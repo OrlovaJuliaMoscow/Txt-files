9961a50c137b8c4903c9e8faebd13ffe581eab30
orlova.julia.ivan@gmail.com <orlova.julia.ivan@gmail.com>
2023-02-06 16:36:43 +0300

    Добавлен Задание 2_Сумма чисел

---
+++ Задание2_Сумма чисел/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание2_Сумма_чисел
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"F:\C#_temp\SumNumbers.txt";
+            Random rand = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(rand.Next(-10, 10));
+                }
+            }
+            int s = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    s = i++;
+                }
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
